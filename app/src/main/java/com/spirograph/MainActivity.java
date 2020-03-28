3a04be9cca32592cd8ea165816f97b52d10a887b
CharulBhanawat13 <charulbhanawat@ymail.com>
2020-03-28 12:27:44 +0530

    Spinner added

--- app/src/main/java/com/spirograph/MainActivity.java
+++ app/src/main/java/com/spirograph/MainActivity.java
@@ -3,7 +3,9 @@ package com.spirograph;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
 import android.widget.LinearLayout;
+import android.widget.Spinner;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -17,5 +19,13 @@ public class MainActivity extends AppCompatActivity {
         linearLayout = findViewById(R.id.linearLayout);
         SpiroGraphView spiroGraphView = new SpiroGraphView(this);
         linearLayout.addView(spiroGraphView);
+
+
+        Spinner spinner=findViewById(R.id.spinner);
+        String[] items = new String[]{"1", "2", "three"};
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_dropdown_item, items);
+        spinner.setAdapter(adapter);
+
+
     }
 }
